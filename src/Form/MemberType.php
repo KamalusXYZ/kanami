a8f5660db660761e74c55c2eb48375765ee687db
kamalus <formation.kamal.haddou@gmail.com>
2022-05-23 12:08:37 +0200

    Modification du form type pour le member afin de mieux cibler les réponse de l'input, et d'éviter les erreurs. A ce stade je peux creer un memebre mais pas l'integrer a une famille, car rien a été fait au nioveau de la famille, c'eest l'etape suivante.

--- src/Form/MemberType.php
+++ src/Form/MemberType.php
@@ -4,6 +4,12 @@ namespace App\Form;
 
 use App\Entity\Member;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,20 +18,17 @@ class MemberType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('birthday')
-            ->add('phone')
-            ->add('email')
-            ->add('address')
-            ->add('zipCode')
-            ->add('city')
-            ->add('country')
-            ->add('otherAddressDetail')
-            ->add('relationShip')
-            ->add('memberEvent')
-            ->add('MemberDailySession')
-        ;
+            ->add('firstName', TextType::class, ["label" => "Prénom: "])
+            ->add('lastName', TextType::class, ["label" => "Nom: "])
+            ->add('birthday', BirthdayType::class, ['placeholder' => [
+                'year' => 'Année', 'month' => 'Mois', 'day' => 'Jour','label'=>'Date de naissance']])
+            ->add('phone', TelType::class,["label" => "Numéro de téléphone: ",'attr' => ['maxlength' => 10]])
+            ->add('email', EmailType::class, ['label'=>'Adresse email'])
+            ->add('address', TextareaType::class, ['label'=> 'Adresse'])
+            ->add('zipCode', TextType::class,["label" => "Code Postal: ",'attr' => ['maxlength' => 5]])
+            ->add('city', TextType::class, ["label" => "Ville: "])
+            ->add('country', CountryType::class, ["label" => "Prénom: "])
+            ->add('otherAddressDetail');
     }
 
     public function configureOptions(OptionsResolver $resolver): void
