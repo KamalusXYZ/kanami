0796964753e363411c7825e255ae99e4a0ec9b96
kamalus <formation.kamal.haddou@gmail.com>
2022-05-25 16:35:08 +0200

    suppression du formtype installé à l'aide de damien searchitem finalement inutile

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -6,15 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 #[Route('/search')]
 class SearchController extends AbstractController
 {
     #[Route('/item/', name: 'app_search_item', methods: ['POST'])]
     public function itemSearch(Request $request): Response
     {
-//        dd($request->get("word"));
+        $result = $request->get("word");
+
+
         return $this->render('search/index.html.twig', [
             'controller_name' => 'SearchController',
+            'result'=> $result
         ]);
     }
 }
