d90f5912787f322b5f47ae9058fd803d3b1ce7b1
KamalusXYZ <k.haddou123@gmail.com>
2022-06-15 20:53:43 +0200

    amelioration csss modif du twig item pour ajout d icone modif du form de item pour meilleur lisibilité (pas le css)

--- src/Form/ItemType.php
+++ src/Form/ItemType.php
@@ -5,7 +5,10 @@ namespace App\Form;
 use App\Entity\Item;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\LanguageType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,22 +19,60 @@ class ItemType extends AbstractType
     {
         $builder
             ->add('name', TextType::class, ["label" => "Nom: "])
-            ->add('ref', TextType::class, ["label" => "Référence interne à la ludothèque: ",'required' => false])
-            ->add('lang', TextType::class, ["label" => "Langue: ",'required' => false])
-            ->add('publisherGameDuration',TextType::class, ["label" => "Durée d'une partie selon l'éditeur: ",'required' => false])
-            ->add('ourGameDuration',TextType::class, ["label" => "Durée réelle d'une partie: ",'required' => false])
-            ->add('playerNbMin', IntegerType::class,["label" => "Nombre de joueur minimum: ",'required' => false] )
-            ->add('playerNbMax', IntegerType::class,["label" => "Nombre de joueur maximum: ",'required' => false])
-            ->add('ageMin', IntegerType::class,["label" => "Age minimum conseillé par l'éditeur: ",'required' => false])
-            ->add('author', TextType::class, ["label" => "Auteur: ",'required' => false])
-            ->add('illustrator', TextType::class, ["label" => "Illustrateur: ",'required' => false])
-            ->add('publisher', TextType::class, ["label" => "Editeur: ",'required' => false])
-            ->add('itemCondition', TextType::class, ["label" => "Etat d'usure du jeu: ",'required' => false])
-            ->add('completeness', CheckboxType::class, ["label" => "Cochez la case si le jeu est complet: ",'required' => false] )
-            ->add('available', CheckboxType::class, ["label" => "Cochez la case si le jeu est disponible: ",'required' => false])
-            ->add('gamePrice', TextType::class, ["label" => "Valeur en euro du jeu: ",'required' => false])
-            ->add('gameOrigin', TextType::class, ["label" => "Provenance: ",'required' => false])
-            ->add('copyNumber', TextType::class, ["label" => "Remplir uniquement si le jeu est possédé en plusiers exemplaire, indiquez le numéro de l'exemplaire: ",'required' => false]);
+            ->add('ref', TextType::class, ["label" => "Référence interne à la ludothèque: ", 'required' => false])
+            ->add('lang', LanguageType::class, ["label" => "Langue: ", 'required' => false])
+            ->add('publisherGameDuration', ChoiceType::class, [
+                'choices' => [
+
+                    'Moins de 15m' => "Moins de 15m",
+                    'moins de 30m' => "moins de 30m",
+                    'de 30m à 1h' => "de 30m à 1h",
+                    'de 1h à 2h' => "de 1h à 2h",
+                    'de 2h à 3h' => "de 2h à 3h",
+                    'de 3h à 4h' => "de 3h à 4h",
+                    'plus de 4h' => "plus de 4h",
+
+
+                ], "label" => "Durée d'une partie selon l'éditeur: ", 'required' => false
+            ])
+            ->add('ourGameDuration', ChoiceType::class, [
+                'choices' => [
+
+                    'Moins de 15m' => "Moins de 15m",
+                    'moins de 30m' => "moins de 30m",
+                    'de 30m à 1h' => "de 30m à 1h",
+                    'de 1h à 2h' => "de 1h à 2h",
+                    'de 2h à 3h' => "de 2h à 3h",
+                    'de 3h à 4h' => "de 3h à 4h",
+                    'plus de 4h' => "plus de 4h",
+
+
+                ], "label" => "Durée d'une partie selon l'estimation de la ludo: ", 'required' => false
+            ])
+            ->add('playerNbMin', IntegerType::class, ["label" => "Nombre de joueur minimum: ", 'required' => false,])
+            ->add('playerNbMax', IntegerType::class, ["label" => "Nombre de joueur maximum: ", 'required' => false])
+            ->add('ageMin', IntegerType::class, ["label" => "Age minimum conseillé par l'éditeur: ", 'required' => false])
+            ->add('author', TextType::class, ["label" => "Auteur: ", 'required' => false])
+            ->add('illustrator', TextType::class, ["label" => "Illustrateur: ", 'required' => false])
+            ->add('publisher', TextType::class, ["label" => "Editeur: ", 'required' => false])
+            ->add('itemCondition', ChoiceType::class, [
+                'choices' => [
+
+                    'Neuf' => "Neuf",
+                    'Moyen' => "Moyen",
+                    'Usé' => "Usé",
+                    'Très usé' => "Très usé",
+                    'à définir' => "à définir",
+
+
+                ], "label" => "Etat d'usure du jeu: ", 'required' => false
+            ])
+            ->add('completeness', CheckboxType::class, ["label" => "Cochez la case si le jeu est complet: ", 'required' => false])
+            ->add('available', CheckboxType::class, ["label" => "Cochez la case si le jeu est disponible: ", 'required' => false])
+            ->add('gamePrice', MoneyType::class, ["label" => "Valeur en euro du jeu: ", 'required' => false,
+                'empty_data' => 0])
+            ->add('gameOrigin', TextType::class, ["label" => "Provenance: ", 'required' => false])
+            ->add('copyNumber', TextType::class, ["label" => "Remplir uniquement si le jeu est possédé en plusiers exemplaire, indiquez le numéro de l'exemplaire: ", 'required' => false]);
 
     }
 
